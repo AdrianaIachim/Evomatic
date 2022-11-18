9c6196dba082a3d4d5b6cd5ba7b88e7d1e6a2297
AndreaZanella <90708805+AndreaZanella@users.noreply.github.com>
2022-11-18 12:15:08 +0100

    Update Product.php

--- MODELS/Product.php
+++ MODELS/Product.php
@@ -1,7 +1,22 @@
 <?php
-
-class Product{
+class Product
+{
+  
+  //DB Related
+   private $conn;
+   private $table = "product";
+  
+  //Properties
+  public $id;
+  public $name;
+  public $ingredients;
+  public $description;
+  public $prize;
+  
+  //Construct with Db
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
 }
-
-
 ?>
