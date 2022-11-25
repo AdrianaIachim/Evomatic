b4437aa6ab9a1cba0829166e55fd3a9c4f20bf57
AndreaZanella <90708805+AndreaZanella@users.noreply.github.com>
2022-11-25 11:49:14 +0100

    Update Tag.php

--- MODELS/Tag.php
+++ MODELS/Tag.php
@@ -1,41 +1,31 @@
 <?php
-Class Tag
+class Tag
+{
+    protected $conn;
+    protected $table_name = "tag";
+
+    protected $tag_ID;
+    protected $tag;
+
+    public function __construct($db)
     {
-        protected $conn;
-        protected $table_name = "tag";
+        $this->conn = $db;
+    }
 
-        protected $tag_ID;
-        protected $tag;
+    function createTag($tag)
+    {
+        $query = "INSERT INTO $this -> table_name (tag) VALUES (?)";
+        $stmt = $this->conn->prepare($query);
 
-        public function __construct($db)
-        {
-            $this -> conn = $db;
-        }
+        $stmt->bind_param('i', $this->tag);
+        $stmt->execute();
+    }
 
-        function createTag($tag)
-        {
-            $query = "INSERT INTO $this -> table_name (tag) VALUES (?)";   
-            $stmt = $this -> conn -> prepare($query);
-
-            $stmt->bind_param('i', $this -> tag); 
-            $stmt->execute();
-        }
-
-        function getTag($tag_ID)
-        {
-            $query = "SELECT * FROM $this -> table_name WHERE ID = $tag_ID";
-            $stmt = $this -> conn -> prepare($query);
-            return $stmt;
-        }
-
-            foreach(json_decode($products, true) as $product)
-            {
-                $stmt->bind_param('iii', $order_ID, $product['ID'], $product['quantity']); // lega i parametri alla query e le dice quali sono i paramentri
-            foreach(json_decode($products, true) as $product)
-            {
-                $stmt->bind_param('iii', $order_ID, $product['ID'], $product['quantity']); // lega i parametri alla query e le dice quali sono i paramentri
-                $stmt->execute();
-            }
-        }
-    ?>
-
+    function getTag($tag_ID)
+    {
+        $query = "SELECT * FROM $this -> table_name WHERE ID = $tag_ID";
+        $stmt = $this->conn->prepare($query);
+        return $stmt;
+    }
+}
+?>
