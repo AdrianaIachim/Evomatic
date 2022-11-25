91fd03731f4530f05b45fb11fd76dba6ff9a61e8
AndreaZanella <90708805+AndreaZanella@users.noreply.github.com>
2022-11-25 11:47:27 +0100

    Update Tag.php

--- MODELS/Tag.php
+++ MODELS/Tag.php
@@ -27,26 +27,15 @@ Class Tag
             $stmt = $this -> conn -> prepare($query);
             return $stmt;
         }
-        /*
-        function setOrderProduct($order_ID, $products) // setta i prodotti di un determinato ordine
-        {
-            $query = "INSERT INTO $this->table_name (order_ID, product_ID, quantity) VALUES (?, ?, ?)";
-            $stmt = $this->conn->prepare($query);
 
+            foreach(json_decode($products, true) as $product)
+            {
+                $stmt->bind_param('iii', $order_ID, $product['ID'], $product['quantity']); // lega i parametri alla query e le dice quali sono i paramentri
             foreach(json_decode($products, true) as $product)
             {
                 $stmt->bind_param('iii', $order_ID, $product['ID'], $product['quantity']); // lega i parametri alla query e le dice quali sono i paramentri
                 $stmt->execute();
             }
         }
-
-        function getOrderProduct($order_ID) // Ottiene i record della tabella order_product che hanno come order_ID quello passato alla funzione
-        {
-            $query = "SELECT * FROM $this->table_name WHERE order_ID = $order_ID";
-
-            $stmt = $this->conn->query($query);
-
-            return $stmt;
-        }*/
-    }
     ?>
+
