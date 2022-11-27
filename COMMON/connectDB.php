81534abca6c850abb792ff2d228fb95346671829
AndreaZanella <90708805+AndreaZanella@users.noreply.github.com>
2022-11-27 22:49:54 +0100

    Add files via upload

---
+++ COMMON/connectDB.php
@@ -0,0 +1,22 @@
+<?php 
+class Database
+{
+    protected $serverName = "localhost";
+    protected $dbName = "sandwiches";
+    protected $username = "root";
+    protected $password = "";
+    public $conn;
+
+    public function connect()
+    {
+        try
+        {
+            $this->conn = new mysqli($this->serverName, $this->username, $this->password, $this->dbName);
+        }
+        catch (mysqli $ex)
+        {
+            die ("Error connecting to database $ex\n\n");
+        }
+        return $this->conn;
+    }
+}
